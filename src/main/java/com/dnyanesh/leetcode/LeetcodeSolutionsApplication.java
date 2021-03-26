a37828b893e9f670240a6ee43413dcd54b9803b7
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2021-03-26 00:45:28 -0400

    Upated code

--- src/main/java/com/dnyanesh/leetcode/LeetcodeSolutionsApplication.java
+++ src/main/java/com/dnyanesh/leetcode/LeetcodeSolutionsApplication.java
@@ -1,4 +1,4 @@
-package com.dnyanesh.leetcodesolutions;
+package com.dnyanesh.leetcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
