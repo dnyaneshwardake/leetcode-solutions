21d3ab915bbf2f01d615c1723876eef7d7a7a030
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2021-04-01 19:24:40 -0400

    Added two sum code

---
+++ src/main/java/com/dnyanesh/leetcode/TwoSum.java
@@ -0,0 +1,28 @@
+package com.dnyanesh.leetcode;
+
+/*Given an array of integers nums and an integer target,return indices of the two numbers such that they add up to target.
+*You may assume that each input would have exactly one solution,and you may not use the same element twice.
+**/
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 2, 7, 11, 15 };
+		int sum = 18;
+		int[] result = twoSum(array, sum);
+		for (int i : result) {
+			System.out.println(i);
+		}
+	}
+
+}
