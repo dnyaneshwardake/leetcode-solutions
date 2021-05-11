ec0b7d15e02187241f3b9c3ea89b95235f6b21b2
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2021-05-11 00:04:16 -0400

    Update MedianSortedArrays.java

--- src/main/java/com/dnyanesh/leetcode/MedianSortedArrays.java
+++ src/main/java/com/dnyanesh/leetcode/MedianSortedArrays.java
@@ -4,36 +4,46 @@ public class MedianSortedArrays {
 
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int[] finalArray = new int[nums1.length + nums2.length];
+		if (finalArray.length == 0) {
+			return 0.0;
+		}
 		int count1 = 0;
 		int count2 = 0;
 		for (int i = 0; i < finalArray.length; i++) {
 
-			if (!(count2 == nums1.length - 1) && !(count1 == nums2.length - 1) && nums1[i] == nums2[i]) {
-				finalArray[i] = nums2[i];
-				finalArray[i + 1] = nums1[i];
+			if (count1 < nums1.length && count2 < nums2.length && nums1[count1] == nums2[count2]) {
+				finalArray[i] = nums1[count1];
 				count1++;
+				finalArray[++i] = nums2[count2];
 				count2++;
-			} else if (!(count1 == nums2.length - 1) && nums1[i] > nums2[i]) {
+				continue;
+			}
+			if (count1 < nums1.length && (count2 == nums2.length || nums1[count1] < nums2[count2])) {
+				finalArray[i] = nums1[count1];
 				count1++;
-				finalArray[i] = nums2[i];
-			} else if (!(count2 == nums1.length - 1) && nums1[i] < nums2[i]) {
+				continue;
+			} else if (count2 < nums2.length && (count1 == nums1.length || nums1[count1] > nums2[count2])) {
+				finalArray[i] = nums2[count2];
 				count2++;
-				finalArray[i] = nums1[i];
+				continue;
 			}
 		}
-		for (int i = 0; i < finalArray.length; i++) {
-			System.out.print(" " + finalArray[i]);
+
+		if (finalArray.length % 2 == 0) {
+			double x = finalArray[finalArray.length / 2 - 1];
+			double y = finalArray[finalArray.length / 2];
+			return (x + y) / 2;
+		} else {
+			return finalArray[finalArray.length / 2];
 		}
 
-		return 1;
 	}
 
 	public static void main(String[] args) {
-
-		int[] num1 = { 1, 2 };
-		int[] num2 = { 3, 4 };
+		int[] num1 = { 2, 2, 4, 4, 5 };
+		int[] num2 = { 2, 2, 4, 4 };
 		double median = findMedianSortedArrays(num1, num2);
-		System.out.println(median);
+		System.out.println(" Median: " + median);
 	}
 
 }
