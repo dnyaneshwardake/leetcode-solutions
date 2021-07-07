6573f1367990c6a437793f0b8c54cd3c0da0d0ef
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2021-07-07 16:54:28 -0400

    Create ListNode.java

---
+++ src/main/java/com/dnyanesh/leetcode/ds/linkedlist/ListNode.java
@@ -0,0 +1,13 @@
+package com.dnyanesh.leetcode.ds.linkedlist;
+
+public class ListNode {
+	int data;
+	ListNode next;
+
+	public ListNode() {
+	}
+
+	public ListNode(int data) {
+		this.data = data;
+	}
+}
